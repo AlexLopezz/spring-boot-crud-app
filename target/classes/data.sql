c67fa508b83670d781d69cadd1306322a99fbb96
alex <alexxxd23@gmail.com>
2024-02-15 21:00:52 -0300

    add .gitignore

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO clients(name, last_name, email, created_at) VALUES('Juergen', 'Hoeller', 'springboot@gmail.com', '2000-10-10');
-INSERT INTO clients(name, last_name, email, created_at) VALUES('Linus', 'Torvals', 'linux@gmail.com', '2000-10-10');
-INSERT INTO clients(name, last_name, email, created_at) VALUES('Bill', 'Gates', 'microsoft@gmail.com', '2000-10-10');
-INSERT INTO clients(name, last_name, email, created_at) VALUES('Elon', 'Musk', 'tesla@gmail.com', '2000-10-10');
-INSERT INTO clients(name, last_name, email, created_at) VALUES('Mark', 'Zuckerberg', 'facebook@gmail.com', '2000-10-10');
